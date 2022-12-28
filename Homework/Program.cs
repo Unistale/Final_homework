9e3eb402fb60860614541b96975c2dec11c701e7
Dmitriy <Unistale@mail.ru>
2022-12-28 11:24:23 +0500

    added void SecondArray

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,16 @@
-﻿string[] array1 = new string[6] { "1997", "74", "pork", "sin", "owl", "cat"};
+﻿string[] array1 = new string[6] { "1997", "74", "pork", "sin", "owl", "cat" };
 string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
